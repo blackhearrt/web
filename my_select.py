909e4175024635acae9a1d35d70af5f549614ecf
blackhearrt <svmamonov36@gmail.com>
2024-09-09 11:10:22 +0300

    making selection functions

---
+++ my_select.py
@@ -0,0 +1,29 @@
+def select_1():
+    pass
+
+def select_2():
+    pass
+
+def select_3():
+    pass    
+
+def select_4():
+    pass    
+
+def select_5():
+    pass
+
+def select_6():
+    pass
+
+def select_7():
+    pass
+
+def select_8():
+    pass
+
+def select_9():
+    pass
+
+def select_10():
+    pass
